3ed44b453292ee1323f020fe86b80dbb6eca7b48
Dxd4 <46905365+Dxd4@users.noreply.github.com>
2022-09-27 17:59:35 +0000

    Update Work_with_files.py

--- src/Work_with_files.py
+++ src/Work_with_files.py
@@ -1,7 +1,7 @@
 import csv
 
 with open("Names_and_phones.csv", newline='') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=';')
+	spamreader = csv.reader(csvfile, delimiter=',')
 	file = list(spamreader)
 	index = int(input(f'Write number of column: {", ".join(file[0])}'))-1
 	with open("output.txt", "w") as output:
